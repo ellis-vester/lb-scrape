7194089498b642857ee81ef1bd5626fc873127ef
ellis-vester <ellis_vester@protonmail.com>
2024-05-24 21:28:57 -0400

    Added version number to root command

--- cmd/root.go
+++ cmd/root.go
@@ -4,51 +4,20 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var Verbose bool
-var Debug bool
+var Version string = "v0.1.0"
 
 var rootCmd = &cobra.Command{
-	Use:   "lbs",
-	Short: "A tool for scraping and aggregating data on letterboxd.com.",
-	Long:  `.`,
+	Use:     "lbs",
+	Short:   "A tool for scraping and aggregating data from letterboxd.com.",
+	Long:    `.`,
+	Version: Version,
 }
 
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
-	}
-}
-
-func init() {
-	rootCmd.Flags().BoolP(
-		"toggle",
-		"t",
-		false,
-		"Help message for toggle")
-
-	rootCmd.PersistentFlags().BoolVarP(
-		&Verbose,
-		"verbose",
-		"v",
-		false,
-		"Display more verbose output in console output.")
-	err := viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup(("verbose")))
-	if err != nil {
-		panic(err)
-	}
-
-	rootCmd.PersistentFlags().BoolVarP(
-		&Debug,
-		"debug",
-		"d",
-		false,
-		"Display debug output in console output.")
-	err = viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup(("debug")))
-	if err != nil {
-		panic(err)
 	}
 }
